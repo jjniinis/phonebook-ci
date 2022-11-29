9728ac85425bb8e324f76b6569c95fe9d16e77ac
Juha Niinistö <juha.niinisto@gmail.com>
2022-11-29 18:39:07 +0200

    Changed default port.

--- src/services/communications.js
+++ src/services/communications.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const PORT = process.env.REACT_APP_PORT || 3001
+const PORT = process.env.REACT_APP_PORT || 8080
 const baseUrl = `http://localhost:${PORT}/api/persons`
 console.log(`env port value: ${process.env.REACT_APP_PORT}`)
 console.log(`baseUrl: ${baseUrl}`)
